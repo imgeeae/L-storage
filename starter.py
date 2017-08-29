3f433a01d679ee4fad2cc69bd63ce77cc0ba6f18
KOR\ellena.lim <tomojiae@gmail.com>
2017-08-29 22:00:54 +0900

    Add an authorship information

---
+++ starter.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example of python file using unicode
+
+We need to write down somethings for using unicode like 'Han-geul'
+"""
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+__author__ = "Ellena Jiae Lim"
+__copyright__ = "Copyright 2017, Mackevision Korea"
+__credits__ = "Ellena Jiae Lim"
+
+__license__ = "GPL"
+__version__ = "1.1.0"
+__maintainer__ = "Ellena Jiae Lim"
+__email__ = "ellena.lim@mackevision.com"
+__status__ = "Production - PDM"
+
+"""
+# Get current time (yyyy-mm-dd_hh_mm)
+now = str(datetime.datetime.now())
+now = now[0:16]
+now = str.replace(now, " ", "_")
+now = str.replace(now, ":", "_")
+
+# Get current directory
+here = os.getcwd()
+
+print now
+print here
+
+print sys.executable
+print sys.path
+"""
