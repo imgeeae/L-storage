26b6feabd838dfc288818ef7cfee47e0afdd3e19
KOR\ellena.lim <tomojiae@gmail.com>
2017-08-29 20:20:21 +0900

    Add a python example

---
+++ starter.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Information of this python file using unicode
+
+You need to write down somethings for using unicode like 'Han-geul'.
+And you can know when, where, how (interpreter).
+"""
+
+import sys
+import os
+import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# Get current time (yyyy-mm-dd_hh_mm)
+now = str(datetime.datetime.now())
+now = now[0:16]
+now = str.replace(now, " ", "_") # ing.replace()
+now = str.replace(now, ":", "_")
+
+# Get current directory
+here = os.getcwd()
+
+print now
+print here
+print sys.executable
+
+
